bc0e93ae11e3af26db11ad2d5f8ed1a6216b853b
Abdur Rahman Kalim <abdur.rahman.kalim@gmail.com>
2024-04-19 10:09:16 -0400

    wip

--- main.ts
+++ main.ts
@@ -7,18 +7,4 @@ const kavya = new Player("Kavya");
 // Create chess game
 const chess = new Chess(abdur, kavya);
 
-// chess.play();
-
-chess.replay([
-  "a2-a4",
-  "b7-b5",
-  "a4-b5",
-  "a7-a6",
-  "a1-a6",
-  "a8-a6",
-  "b5-a6",
-  "c7-c6",
-  "a6-a7",
-  "c6-c5",
-  "a7-a8", // promotion to a queen
-]);
+chess.play();
